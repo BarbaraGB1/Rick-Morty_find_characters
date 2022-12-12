44c71d8aafc3dd92fe53a43ba052585905538807
BarbaraGB1 <bargarbo@gmail.com>
2022-12-12 20:07:35 +0100

    repository /imports/image

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { Link, NavLink } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { useState , useEffect } from 'react';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className='container'>
+ 
+    </div>
+  );
+}
+
+export default App;
