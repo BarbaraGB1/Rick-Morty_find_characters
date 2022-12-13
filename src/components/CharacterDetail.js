dc371b0430d1a9f6289cde72a9c565fabb5dd696
BarbaraGB1 <bargarbo@gmail.com>
2022-12-13 20:11:40 +0100

    url share

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,15 +3,20 @@ function CharacterDetail(props) {
   const params = useParams();
   const characterFound = props.findCharacter(params.characterId);
   console.log(characterFound);
-  return (
-    <div>
-      <img src={characterFound.image} alt={characterFound.name} />
-      <h2>{characterFound.name}</h2>
-      <h3>Especie: {characterFound.species}</h3>
-      <h3>Planeta: {characterFound.planet}</h3>
-      <h3>{characterFound.status}</h3>
-      <h3>Episodios:{characterFound.episodes.length}</h3>
-    </div>
-  );
+
+  if (characterFound !== undefined) {
+    return (
+      <div>
+        <img src={characterFound.image} alt={characterFound.name} />
+        <h2>{characterFound.name}</h2>
+        <h3>Especie: {characterFound.species}</h3>
+        <h3>Planeta: {characterFound.planet}</h3>
+        <h3>{characterFound.status}</h3>
+        <h3>Episodios:{characterFound.episodes.length}</h3>
+      </div>
+    );
+  } else {
+    return <p>este personaje no existe</p>;
+  }
 }
 export default CharacterDetail;
